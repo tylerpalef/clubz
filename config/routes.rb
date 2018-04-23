e98e76f1886ce25cd0c8be0fa2b6baeb7e197a05
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2018-04-23 12:00:57 -0400

    Solution code

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
   resources :clubs
-  resources :sessions, only: [:new, :create, :destroy]  
+  resource :sessions, only: [:new, :create, :destroy]  
 
   root 'clubs#index'
 
